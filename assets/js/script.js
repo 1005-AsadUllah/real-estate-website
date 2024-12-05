4618085e4afc7a7e87f4726d8fa0c275d3cbdac1
Asad_Ullah <au10050305@gmail.com>
2024-12-05 11:39:21 +0600

    adding all file

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/**
+ * navbar toggle in mobile
+ */
+
+const /**{NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /**{NodeElement} */ $navToggle  = document.querySelector("[data-nav-toggler]");
+
+$navToggle.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/**
+ * Header scroll state
+ */
+const /**{NodeElement} */ $header = document.querySelector("[data-header]")
+
+window.addEventListener("scroll",e =>{
+    $header.classList[window.scrollY > 50 ? "add" :"remove"] ("active");
+});
+
+/** 
+ * add to favorite button toggle
+ */
+const /**{NodeElement} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+$toggleBtns.forEach($toggleBtns =>{
+    $toggleBtns.addEventListener("click",() =>{
+        $toggleBtns.classList.toggle("active");
+    });
+});
